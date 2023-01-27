59378384662a0ac5ce725f54cc9b9cb3536e3050
Ana Visnevskaja <anavisnia@gmail.com>
2023-01-27 16:40:19 +0200

    Added apiInfo in SwggerConfig file

--- src/main/java/com/example/employeedata/config/SwaggerConfig.java
+++ src/main/java/com/example/employeedata/config/SwaggerConfig.java
@@ -6,6 +6,8 @@ import static springfox.documentation.builders.PathSelectors.regex;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -15,10 +17,14 @@ public class SwaggerConfig {
     @Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.OAS_30) //Open Api Spec 3.0
-			.protocols(Set.of("https", "http"))
-			.select()
-			.apis(RequestHandlerSelectors.basePackage("com.example.employeedata.controller"))              
-          	.paths(regex("/api/.*"))  
-			.build();
+		.apiInfo(new ApiInfoBuilder()
+            .title("Employee-Project API")
+            .description("A CRUD API to manage employees and projects")
+            .build())
+		.protocols(Set.of("https", "http"))
+		.select()
+		.apis(RequestHandlerSelectors.basePackage("com.example.employeedata.controller"))              
+		.paths(regex("/api/.*"))  
+		.build();
 	}
 }
